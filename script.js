cc3e8e33728b1d9849aa0a299223cada386561f4
Pignataro67 <Pignataro67@gmail.com>
2021-12-12 10:54:30 -0800

    JS- variables and callling

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const boxesContainer = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
@@ -8,3 +11,5 @@ function createBoxes() {
     }
   }
 }
+
+createBoxes()
